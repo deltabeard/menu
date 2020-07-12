7283c2f6a17596e4f1ec9561620d27723d5c2fa1
Mahyar Koshkouei <mk@deltabeard.com>
2020-07-12 21:14:38 +0100

    menu: add license
    Signed-off-by: Mahyar Koshkouei <mk@deltabeard.com>

--- menu.c
+++ menu.c
@@ -1,29 +1,22 @@
+/**
+ * Copyright (C) 2020 by Mahyar Koshkouei <mk@deltabeard.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
 #include <stdlib.h>
 #include <string.h>
 
 #include "menu.h"
-
-#if 0
-enum item_selection_type_e {
-	/* An array of with nmemb number of
-	 * const char pointers. */
-	ITEM_SEL_STRINGS,
-
-	/* An unsigned value for enable (1) and
-	 * disable (0). nmemb is ignored. */
-	ITEM_SEL_EN_DIS,
-
-	/* An array of three 32-bit signed integers for
-	 * start, step, end. */
-	ITEM_SEL_INT_RANGE,
-
-	/* An array of 32-bit signed integers. The first
-	 * integer is the length of the array, and
-	 * should not be considered a valid input value.
-	 */
-	ITEM_SEL_INT_SPECIFIC
-};
-#endif
 
 struct menu_ctx_s
 {
